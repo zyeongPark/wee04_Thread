b75121f71c3e93f2035be96789db3c8b0da1af9e
zyeongPark <piiy10141997@gmail.com>
2021-10-01 21:57:28 +0900

    Initial Commit

---
+++ wee04_Thread/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wee04_Thread
+//
+//  Created by 박지영 on 2021/10/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
