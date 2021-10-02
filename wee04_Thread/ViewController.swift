3796562f3cfbac2bc70894f5ada0b6ca0621463e
zyeongPark <piiy10141997@gmail.com>
2021-10-02 22:34:20 +0900

    가로모드 고정

--- wee04_Thread/ViewController.swift
+++ wee04_Thread/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //가로모드 고정 부분
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        return .landscapeLeft
+    }
+    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation{
+        return .landscapeLeft
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
